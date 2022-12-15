726d8d78bed842040d1442231ff828d9ec176814
Mohamed-Salih <med.ayoure@gmail.com>
2022-12-15 10:56:00 +0100

    chuck norris

---
+++ app/src/main/java/com/example/androidcloud_salih/yourFeature/repository/AndroidVersionRepository.kt
@@ -0,0 +1,45 @@
+package com.example.androidcloud_salih.yourFeature.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import com.example.androidcloud_salih.architecture.CustomApplication
+import com.example.androidcloud_salih.yourFeature.model.LocalDataSourceSample
+import com.example.androidcloud_salih.yourFeature.model.ObjectDataSample
+
+class AndroidVersionRepository {
+
+    private val mAndroidVersionDao =
+        CustomApplication.instance.mApplicationDatabase.androidVersionDao()
+
+    fun selectAllAndroidVersion(): LiveData<List<ObjectDataSample>> {
+        return mAndroidVersionDao.selectAll().map { list ->
+            list.toObjectDataSample()
+        }
+    }
+
+    fun insertAndroidVersion(objectDataSample: ObjectDataSample) {
+        mAndroidVersionDao.insert(objectDataSample.toRoomObject())
+    }
+
+    fun deleteAllAndroidVersion() {
+        mAndroidVersionDao.deleteAll()
+    }
+}
+
+private fun ObjectDataSample.toRoomObject(): LocalDataSourceSample {
+    return LocalDataSourceSample(
+        name = versionName,
+        code = versionCode,
+        image = versionImage
+    )
+}
+
+private fun List<LocalDataSourceSample>.toObjectDataSample(): List<ObjectDataSample> {
+    return map { eachItem ->
+        ObjectDataSample(
+            versionCode = eachItem.code,
+            versionName = eachItem.name,
+            versionImage = eachItem.image
+        )
+    }
+}
---
+++ app/src/main/java/com/example/androidcloud_salih/yourFeature/viewmodel/AndroidVersionViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.androidcloud_salih.yourFeature.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
+import com.example.androidcloud_salih.yourFeature.model.MyObjectForRecyclerView
+import com.example.androidcloud_salih.yourFeature.model.ObjectDataHeaderSample
+import com.example.androidcloud_salih.yourFeature.model.ObjectDataSample
+import com.example.androidcloud_salih.yourFeature.repository.AndroidVersionRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AndroidVersionViewModel : ViewModel() {
+
+    private val androidVersionRepository: AndroidVersionRepository by lazy { AndroidVersionRepository() }
+    val androidVersionList: LiveData<List<MyObjectForRecyclerView>> = androidVersionRepository.selectAllAndroidVersion().map { list ->
+            list.toMyObjectForRecyclerView()
+        }
+
+
+    fun insertAndroidVersion(androidName: String, androidCode: Int, url: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            androidVersionRepository.insertAndroidVersion(
+                ObjectDataSample(androidName, androidCode, url)
+            )
+        }
+    }
+
+    fun deleteAllAndroidVersion() {
+        viewModelScope.launch(Dispatchers.IO) {
+            androidVersionRepository.deleteAllAndroidVersion()
+        }
+    }
+}
+
+private fun List<ObjectDataSample>.toMyObjectForRecyclerView(): List<MyObjectForRecyclerView> {
+    val result = mutableListOf<MyObjectForRecyclerView>()
+
+    groupBy {
+        // Split in 2 list, modulo and not
+        it.versionCode % 2 == 0
+    }.forEach { (isModulo, items) ->
+        // For each mean for each list split
+        // Here we have a map (key = isModulo) and each key have a list of it's items
+        result.add(ObjectDataHeaderSample("Is modulo : $isModulo"))
+        result.addAll(items)
+    }
+    return result
+}
